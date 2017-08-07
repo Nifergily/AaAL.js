1e966823d9c33d7382d4c687338135df90e131a0
Arseny Maslennikov <ar@cs.msu.ru>
2017-08-08 02:13:11 +0300

    NAM.js: refactor

--- NAM.js
+++ NAM.js
@@ -1,137 +1,145 @@
+// pythonic "".format
+// TODO: ECMAScript 2015 defines template strings
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/template_strings
+// https://habrahabr.ru/post/313526/
+// Browser support seems pretty good
 if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
+    String.prototype.format = function() {
+        let args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+            ;
+        });
+    };
 }
 
-function HighLightCode ( Code, Line )
+function loadAlg(word, alg)
 {
-  var Area = document.getElementById ("ParsedCode");
-  var Alpha, Betha, Arrow, Dummy;
-  Dummy = "<table width=100% cellspacing=0>";
-  for ( i = 0; i < Code.length; ++i )
-  {
-    var Rule = Code[i];
-    var pos = Rule.indexOf ( "->" );
-    var BGCol = " ";
-    if ( i == Line ) BGCol += " style=\"background-color:#c0c0d0;\"";
-
-    if ( pos < 0 ) pos = Rule.indexOf ( "=>" );
-    if ( 0 <= pos )
-    {
-      Alpha = Rule.substr ( 0, pos );
-      Arrow = Rule.substr ( pos, 2 );
-      if ( Arrow == "->" ) Arrow = "&#8594"; else Arrow = "↦";
-      Betha = Rule.substr ( pos + 2, Rule.length - pos - 2 );
-    }
-    Dummy += "<tr valign=top {0}><td {0} align=right>{1}.</td><td {0} align=right>{2}</td><td {0} align=center>{3}</td><td {0} align=left>{4}</td></tr>".format(BGCol,i+1,Alpha,Arrow,Betha);
-  }
-  Dummy += "</table>";
-  Area.innerHTML = Dummy;
+    document.getElementById("ErrorMsg").innerHTML = "Загружена запись алгоритма.\n";
+    document.getElementById("Word").value = word;
+    document.getElementById("CodeArea").value = alg;
+    document.getElementById("ParsedCode").innerHTML = "<table width=100%><tr><td> </td></tr></table>";
 }
 
-function RunOnce ()
+function highlightCode(code, line)
 {
-  var RulesMaxQty = 50;
-  var Res = false;
-  var Executed = 0;
-  var Code = document.getElementById ("CodeArea").value.split (/\n/);
-  var Word = document.getElementById ("Word").value;
-  var Rules = new Array ();
-  var TerminalRule;
-  var pos;
-  var alpha, betha;
-  var c;
+    let area = document.getElementById("ParsedCode");
+    let alpha, betha, arrow, dummy;
+    dummy = "<table width=100% cellspacing=0>";
+    for (i = 0; i < code.length; ++i)
+    {
+        let rule = code[i];
+        let pos = rule.indexOf("->");
+        let BGCol = " ";
+        if (i == line) BGCol += " style=\"background-color:#c0c0d0;\"";
 
-  R = 0;
-  for ( i = 0; i < Code.length && i < RulesMaxQty; ++i )
-  {
-    var tmp = Code[i];
-    var Rule = "";
-    for ( j = 0; j < tmp.length; ++j )
-    {
-      c = tmp.substr(j,1);
-      if ( c != " " && c != "\t" ) Rule += c;
+        if (pos < 0) pos = rule.indexOf( "=>" );
+        if (0 <= pos)
+        {
+            alpha = rule.substr(0, pos);
+            arrow = rule.substr(pos, 2);
+            if (arrow == "->") arrow = "&#8594"; else arrow = "↦";
+            betha = rule.substr(pos + 2, rule.length - pos - 2);
+        }
+        dummy += "<tr valign=top {0}><td {0} align=right>{1}.</td><td {0} align=right>{2}</td><td {0} align=center>{3}</td><td {0} align=left>{4}</td></tr>".format(BGCol, i+1, alpha, arrow, betha);
     }
-    if ( 0 < Rule.length )
+    dummy += "</table>";
+    area.innerHTML = dummy;
+}
+
+function runOnce()
+{
+    let rulesMaxQty = 50;
+    let res = false;
+    let executed = 0;
+    let code = document.getElementById("CodeArea").value.split(/\n/);
+    let word = document.getElementById("Word").value;
+    let rules = new Array();
+    let terminalRule;
+    let pos;
+    let alpha, betha;
+    let c;
+
+    R = 0;
+    for (let i = 0; i < code.length && i < rulesMaxQty; ++i)
     {
-       if ( Rule.indexOf ( "->" ) < 0 && Rule.indexOf ( "=>" ) < 0 )
-         document.getElementById ("ErrorMsg").innerHTML += ( i + 1 ) + ": пропущен знак правила.\n";
-       else
-       {
-         Rules[R] = Rule;
-         ++R;
-       }
+        let tmp = code[i];
+        let rule = "";
+        for (let j = 0; j < tmp.length; ++j)
+        {
+            c = tmp.substr(j, 1);
+            if (c != " " && c != "\t") rule += c;
+        }
+        if (0 < rule.length)
+        {
+            if (rule.indexOf ("->") < 0 && rule.indexOf ("=>") < 0)
+                document.getElementById("ErrorMsg").innerHTML += (i + 1) + ": пропущен знак правила.\n";
+            else
+            {
+                rules[R] = rule;
+                ++R;
+            }
+        }
+        else
+            document.getElementById("ErrorMsg").innerHTML += (i + 1) + ": пустая строка.\n";
     }
+
+    i = 0;
+    totalRules = R <= 32 ? R : 32;
+    terminalRule = false;
+    while (executed == 0 && i < totalRules && !terminalRule)
+    {
+        terminalRule = false;
+        rule = rules[i];
+        if (0 <= (pos = rule.indexOf("->")))
+        {
+            alpha = rule.substr(0, pos);
+            betha = rule.substr(pos + 2, rule.length - pos - 2);
+            if (0 <= word.indexOf(alpha))
+            {
+                document.getElementById("Word").value = word.replace(alpha, betha);
+                executed = i + 1;
+            }
+        }
+        else if (0 <= (pos = rule.indexOf("=>")))
+        {
+            alpha = rule.substr(0, pos);
+            betha = rule.substr(pos + 2, rule.length - pos - 2);
+            if ( 0 <= word.indexOf(alpha))
+            {
+                document.getElementById("Word").value = word.replace(alpha, betha);
+                terminalRule = true;
+                executed = i + 1;
+            }
+        }
+        if (executed) highlightCode(rules, i);
+        ++i;
+    }
+    return (executed != 0 && !terminalRule) ? executed : 0;
+}
+
+function run()
+{
+    let maxExecutions  = 1000;
+    let executionCount = 0;
+    let Applicable     = true;
+
+    document.getElementById("ErrorMsg").innerHTML = "";
+    while (runOnce() && (executionCount < maxExecutions)) { ++executionCount; }
+    if (executionCount == maxExecutions)
+        document.getElementById("ErrorMsg").innerHTML += "Превышено время выполнения.\n";
     else
-      document.getElementById ("ErrorMsg").innerHTML += ( i + 1 ) + ": пустая строка.\n";
-  }
-
-  i = 0;
-  TotalRules = R <= 32 ? R : 32;
-  TerminalRule = false;
-  while ( Executed == 0 && i < TotalRules && !TerminalRule )
-  {
-    TerminalRule = false;
-    Rule = Rules[i];
-    if ( 0 <= ( pos = Rule.indexOf ( "->" ) ) )
-    {
-      alpha = Rule.substr ( 0, pos );
-      betha = Rule.substr ( pos + 2, Rule.length - pos - 2 );
-      if ( 0 <= Word.indexOf (alpha) )
-      {
-        document.getElementById ("Word").value = Word.replace ( alpha, betha );
-        Executed = i + 1;
-      }
-    }
-    else if ( 0 <= ( pos = Rule.indexOf ( "=>" ) ) )
-    {
-      alpha = Rule.substr ( 0, pos );
-      betha = Rule.substr ( pos + 2, Rule.length - pos - 2 );
-      if ( 0 <= Word.indexOf (alpha) )
-      {
-        document.getElementById ("Word").value = Word.replace ( alpha, betha );
-        TerminalRule = true;
-        Executed = i + 1;
-      }
-    }
-    if ( Executed ) HighLightCode ( Rules, i );
-    ++i;
-  }
-  return ( Executed != 0 && !TerminalRule ) ? Executed : 0;
+        document.getElementById("ErrorMsg").innerHTML += "Выполнение завершено.\n";
 }
 
-function Run ()
+function makeStep()
 {
-  var MaxExecution   = 1000;
-  var ExecutionCount = 0;
-  var Applicable     = true;
-
-  document.getElementById ("ErrorMsg").innerHTML = "";
-  while ( RunOnce () && ExecutionCount < MaxExecution ) { ++ExecutionCount; }
-  if ( ExecutionCount == MaxExecution )
-    document.getElementById ("ErrorMsg").innerHTML += "Превышено время выполнения.\n";
-  else
-    document.getElementById ("ErrorMsg").innerHTML += "Выполнение завершено.\n";
+    let errorMessage = document.getElementById("ErrorMsg");
+    if (r = runOnce())
+        errorMessage.innerHTML += "Правило " + r + " применено.\n";
+    else
+        errorMessage.innerHTML = "Ни одно правило не применено, либо выполнено терминальное правило.\n";
 }
 
-function Step ()
-{
-  var ErrorMSg = document.getElementById ("ErrorMsg");
-  if ( r = RunOnce() ) ErrorMSg.innerHTML += "Правило " + r + " применено.\n";
-  else ErrorMSg.innerHTML = "Ни одно правило не применено, либо выполнено терминальное правило.\n";
-}
-
-function LoadAlg ( Word, Alg )
-{
-  document.getElementById ("ErrorMsg").innerHTML = "Загружена запись алгоритма.\n";
-  document.getElementById ("Word").value = Word;
-  document.getElementById ("CodeArea").value = Alg;
-  document.getElementById ("ParsedCode").innerHTML = "<table width=100%><tr><td> </td></tr></table>";
-}
